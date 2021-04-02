2d72c40fecd062227dd4942c93e679693b58638f
Sylvan Ricardo <46041980+PowderHan@users.noreply.github.com>
2021-04-02 09:16:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'Sylvan's Blog',
+  author: 'Sylvan Ricardo',
+  email: 'jiangzhh25@mail2.sysu.edu.cn',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
